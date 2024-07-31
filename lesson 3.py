46130a86f4a8e7815c3dcaaa9d208d6c1a4afc6d
Demyan k. <demiandiaz.dd@gmail.com>
2024-07-31 13:32:17 +0300

    Add files via upload

---
+++ lesson 3.py
@@ -0,0 +1,15 @@
+print(type(5)) # inte = integer = целые числа
+print(type(2.0)) # float = плавушия точка
+print(type('Hello, world')) # str = string = срока (текст)
+print('Hello, "World"') # test
+print('Hello ' + 'World') # concatenate
+print(type(True), type(False)) # bool = boolean = логический тип данных
+print(5 != 5 and 5 < 10) # test (false) строгий
+print(5 != 5 or 5 < 10)    # test (true) не строгий
+
+# перевисти тип данных
+print(type(int('5'))) # convert from str to int
+print(type(str('5'))) # convert from str to str
+print(type(float('5'))) # convert from str to float
+print(type(bool('5'))) # convert from str to bool
+
