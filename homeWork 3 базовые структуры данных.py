f4ae51c8b4e6066a23c2457409bf8d416c238dc9
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-02 16:12:20 +0300

    Add files via upload

---
+++ homeWork 3 базовые структуры данных.py
@@ -0,0 +1,34 @@
+# 1st operation
+
+num1 = 9
+num2 = 0.5
+num3 = 5
+
+print((num1 ** num2) * num3)
+
+# 2nd program
+
+num1 = 9.99
+num2 = 9.98
+num3 = 1000
+num4 = 1000.1
+
+print(num1 > num2 and num3 != num4)
+
+# 3rd program
+
+num1 = 2
+num2 = 2
+num3 = 2
+
+print(num1 * num2 + num3 == num1 * (num2 + num3))
+
+# 4th programm
+
+num1 = '123.456'
+
+print(num1[4])
+
+print(type(float(num1)))
+
+print(int(10 * 1234.56))
