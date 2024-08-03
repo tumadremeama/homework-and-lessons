659b8baaa323b54bb218cb9166de17eb931b062c
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-03 17:36:35 +0300

    Update HomeWork 4 переменные.py

--- HomeWork 4 переменные.py
+++ HomeWork 4 переменные.py
@@ -1,7 +1,7 @@
 course = 'Python'
 total_tasks = 12
 total_hours = 1.5
-average_execution_time = total_tasks / total_hours
+average_execution_time = total_hours / total_tasks
 print(average_execution_time)
 
 print('В курсе', course,'было всего',total_tasks ,'задач', 'на', total_hours, 'затраченого часов', 'в среднее время выполненя', average_execution_time, 'минут')
