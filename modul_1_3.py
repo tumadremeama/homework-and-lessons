0e582bfb2092fe9b6f5a1562dee420ddc1f7e419
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-04 13:44:49 +0300

    Update modul_1_3.py

--- modul_1_3.py
+++ modul_1_3.py
@@ -2,7 +2,7 @@ name = 'Demyan'
 print(name, type(name))
 age = 28
 print(age, type(age))
-new_age = 29
-print(new_age, type(new_age))
+age = 29
+print(age, type(age))
 is_student = True
 print(is_student, type(is_student))
