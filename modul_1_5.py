f5bbe33edf42f4e62ebf3978dfabb036b7602f0a
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-05 16:34:35 +0300

    Add files via upload

---
+++ modul_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = (1 ,23 , 'alpha', 155, 32, False)
+print(immutable_var)
+# immutable_var[2] = 2
+# кортеж не поддерживает обрашение по элементам
+mutable_list = ['alpha',14 , 16, 'expert', '.com' ]
+print(mutable_list)
+mutable_list[3] = 'secret'
+print(mutable_list)
