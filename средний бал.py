c235d33b5e6119a72f8724e8f78ad96a1ff5e37e
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-07 10:35:24 +0300

    Add files via upload

---
+++ средний бал.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+print((5 + 3 + 3 + 5 + 4) / 5) # for Johnny
+print((2 + 2 + 2 + 3) / 4 )  # for Bilbo
+print((4 + 5 + 5 + 2) / 4)   # for Steve
+print((4 + 4 + 3) / 3)       # for Khendrik
+print((5 + 5 + 5 + 4 + 5) / 5) # for Aaron
+
+
+Notes = {'Johnny': 4.0, 'Bilbo': 2.25, 'Steve': 4.0, 'Khendrik': 3.6666666666666665, 'Aaron': 4.8}  
