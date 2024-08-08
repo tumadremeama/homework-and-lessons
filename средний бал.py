9e691992ae2759627bff8e8a4e1f9535f3040ce8
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-08 12:55:42 +0300

    Update средний бал.py

--- средний бал.py
+++ средний бал.py
@@ -1,10 +1,20 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-print((5 + 3 + 3 + 5 + 4) / 5) # for Johnny
-print((2 + 2 + 2 + 3) / 4 )  # for Bilbo
-print((4 + 5 + 5 + 2) / 4)   # for Steve
-print((4 + 4 + 3) / 3)       # for Khendrik
-print((5 + 5 + 5 + 4 + 5) / 5) # for Aaron
+num = [5, 3, 3, 5, 4] # for Johnny
+med = sum(num) / len(num)
+print(med)
+num = [2, 2, 2, 3]  # for Bilbo
+med = sum(num) / len(num)
+print(med)
+num = [4, 5, 5, 2]  # for Steve
+med = sum(num) / len(num)
+print(med)
+num = [4, 4, 3]  # for Khendrik
+med = sum(num) / len(num)
+print(med)
+num = [5, 5, 5, 4, 5] # for Aaron
+med = sum(num) / len(num)
+print(med)
 
 
 Notes = {'Johnny': 4.0, 'Bilbo': 2.25, 'Steve': 4.0, 'Khendrik': 3.6666666666666665, 'Aaron': 4.8}  
