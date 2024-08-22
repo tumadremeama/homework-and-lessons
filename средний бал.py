ce1488514cc886b0e15895c04cbf6ea63bcae868
Demyan k. <demiandiaz.dd@gmail.com>
2024-08-22 14:34:00 +0300

    Update средний бал.py

--- средний бал.py
+++ средний бал.py
@@ -1,20 +1,11 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-num = [5, 3, 3, 5, 4] # for Johnny
-med = sum(num) / len(num)
-print(med)
-num = [2, 2, 2, 3]  # for Bilbo
-med = sum(num) / len(num)
-print(med)
-num = [4, 5, 5, 2]  # for Steve
-med = sum(num) / len(num)
-print(med)
-num = [4, 4, 3]  # for Khendrik
-med = sum(num) / len(num)
-print(med)
-num = [5, 5, 5, 4, 5] # for Aaron
-med = sum(num) / len(num)
-print(med)
 
+students_list = sorted(students)
+average_grades = {}
 
-Notes = {'Johnny': 4.0, 'Bilbo': 2.25, 'Steve': 4.0, 'Khendrik': 3.6666666666666665, 'Aaron': 4.8}  
+for i, student in enumerate(students_list):
+    average = sum(grades[i]) / len(grades[i])
+    average_grades[student] = average
+
+print(average_grades) 
